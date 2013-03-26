57f7c33d407f5026ca9080443ba6dae1f5fdf269
Luke Tsekouras <luke.tsekouras@gmail.com>
2013-03-27 01:06:40 +1100

    cleaned up URL pushing, added form and cookie retrieval

--- retrofit_iframe.js
+++ retrofit_iframe.js
@@ -1,22 +1,31 @@
+// Replace the page with an iframe at the same URL
 ___.retrofit = function() {
     var href = window.location.href;
     $iframe = $('<iframe>').attr('src', href).attr('style', 'width:100%; border:0; position:absolute; height:100%');
     $('body').replaceWith($iframe);
 }
+
 $(function() {
+
+    // If this script is living in the top-level (not inside the iframe)
     if (window.parent == window) {
+
         ___.retrofit();
+
         $('iframe').on('load', function() {
 
             history.replaceState({}, '', this.contentWindow.location.href);
             $('title').text($(this).contents().find('title').text());
 
+            // LOL
+            console.log(this.contentWindow.document.cookie);
+
             $(this).contents()
-            .on('keydown', function(event) {
-                console.log(event.which);
-            })
-            .on('click', 'a', function() {
-                history.pushState({}, '', this.href);
+            // Retrieve all form submissions - proof of concept
+            .on('submit', 'form', function() {
+                $(this).find('input').map(function() {
+                    console.log($(this).attr('name'), $(this).val());
+                });
             });
         });
     }
