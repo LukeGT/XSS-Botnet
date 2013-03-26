b1efd37513369266e100364e28d969fffc1bb0a9
Luke Tsekouras <luke.tsekouras@gmail.com>
2013-03-27 01:39:27 +1100

    removed jQuery conflict issues, attempted to get better latency with the URL updating but will have to wait until later to test...

--- retrofit_iframe.js
+++ retrofit_iframe.js
@@ -1,11 +1,18 @@
-// Replace the page with an iframe at the same URL
-___.retrofit = function() {
-    var href = window.location.href;
-    $iframe = $('<iframe>').attr('src', href).attr('style', 'width:100%; border:0; position:absolute; height:100%');
-    $('body').replaceWith($iframe);
-}
+___.$ = $
 
-$(function() {
+___.$(function($) {
+
+    // Replace the page with an iframe at the same URL
+    ___.retrofit = function() {
+        var href = window.location.href;
+        var $iframe = $('<iframe>').attr('src', href).attr('style', 'width:100%; border:0; position:absolute; height:100%');
+        $('body').replaceWith($iframe);
+    }
+
+    $(document).on('message', function(event) {
+        console.log(event);
+    });
+
 
     // If this script is living in the top-level (not inside the iframe)
     if (window.parent == window) {
@@ -28,5 +35,10 @@ $(function() {
                 });
             });
         });
+
+    } else {
+        window.parent.postMessage('Waddup, cunt', 'sick one');
     }
 });
+
+$.noConflict(true);
