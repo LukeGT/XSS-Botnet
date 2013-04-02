605c8448574f8e10fce44936102a87e1519dd3fb
Luke Tsekouras <luke.tsekouras@gmail.com>
2013-04-03 00:24:07 +1100

    no jquery conflict

--- retrofit_pushState.js
+++ retrofit_pushState.js
@@ -1,31 +1,36 @@
-___.load = function(href) {
-    $.ajax({
-        url: href,
-        success: function(data) {
-            var $data = $(data);
-            $('html').replaceWith($data);
-            // $('html')[0].innerHTML = data.replace(/^\s*<\s*html\s*>/i, '').replace(/<\/\s*html\s*>\s*$/i, '');
-            ___.retrofit();
-        }
-    });
-}
-___.push = function(href) {
-    history.pushState({}, '', href);
-    ___.load(href);
-};
-___.retrofit = function() {
-    $('a').each(function() {
-        var $this = $(this);
-        var href = $this.attr('href');
-        if (href.match(/^http/)) {
-            return;
-        }
-        $this.attr('href', "javascript:___.push('" + href + "')");
-    });
-};
-$(function() {
+___.$ = $;
+
+___.$(function($) {
+    ___.load = function(href) {
+        $.ajax({
+            url: href,
+            success: function(data) {
+                var $data = $(data);
+                $('html')[0].innerHTML = data.replace(/^\s*<\s*html\s*>/i, '').replace(/<\/\s*html\s*>\s*$/i, '');
+                ___.retrofit();
+            }
+        });
+    }
+    ___.push = function(href) {
+        history.pushState({}, '', href);
+        ___.load(href);
+    };
+    ___.retrofit = function() {
+        $('a').each(function() {
+            var $this = $(this);
+            var href = $this.attr('href');
+            if (href) {
+                if (href.match(/^http/)) {
+                    return;
+                }
+                $this.attr('href', "javascript:___.push('" + href + "')");
+            }
+        });
+    };
     ___.retrofit();
     window.onpopstate = function(event) {
         ___.load(window.location.href);
     }
 });
+
+$.noConflict(true);
