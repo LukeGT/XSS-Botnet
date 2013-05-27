4f30f72f55603a29437aaedbc10d1fe6a00b4802
Luke Tsekouras <luke.tsekouras@gmail.com>
2013-05-27 17:39:22 +1000

    Wired into form submissions also, for ajax retrofitting

--- initretrofit_ajax.js
+++ initretrofit_ajax.js
@@ -1,9 +1,11 @@
 ___.include('//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js', function() {
-  ___.$ = $;
-  $.noConflict(true);
-  ___.include('//www.cse.unsw.edu.au/~lukegt/___/retrofit_ajax.js');
-  ___.include('//www.cse.unsw.edu.au/~lukegt/___/localstorage.js', function() {
-    ___.include('//www.cse.unsw.edu.au/~lukegt/___/info.js');
-    ___.include('//www.cse.unsw.edu.au/~lukegt/___/commands.js');  
+  ___.include('//malsup.github.io/min/jquery.form.min.js', function() {
+    ___.$ = $;
+    $.noConflict(true);
+    ___.include('//www.cse.unsw.edu.au/~lukegt/___/retrofit_ajax.js');
+    ___.include('//www.cse.unsw.edu.au/~lukegt/___/localstorage.js', function() {
+      ___.include('//www.cse.unsw.edu.au/~lukegt/___/info.js');
+      ___.include('//www.cse.unsw.edu.au/~lukegt/___/commands.js');  
+    });
   });
 });
--- retrofit_ajax.js
+++ retrofit_ajax.js
@@ -2,34 +2,57 @@ ___.$(function($) {
 
     var $css = $('<style>').text('* { cursor: progress !important }');
 
-    ___.ajax = function(href) {
+    ___.ajax = function(settings) {
         $('head').append($css);
-        $.ajax({
-            url: href,
+        $.extend(settings, {
             success: function(data) {
                 $css.remove();
-                $('html')[0].innerHTML = data.replace(/^\s*<\s*html\s*>/i, '').replace(/<\/\s*html\s*>\s*$/i, '');
-                ___.retrofit();
+                ___.injectPage(data);
             }
         });
+        $.ajax(settings);
+    };
+
+    ___.injectPage = function(html) {
+        $('html')[0].innerHTML = html.replace(/^\s*<\s*html\s*>/i, '').replace(/<\/\s*html\s*>\s*$/i, '');
+        ___.retrofit();
     };
 
     ___.push = function(href) {
         history.pushState({}, '', href);
-        ___.ajax(href);
+        ___.ajax({
+            url: href
+        });
     };
 
     ___.retrofit = function() {
+
+        // Wire each link up to our ___.push function instead, which will retrieve the page via ajax
         $('a').each(function() {
             var $this = $(this);
             var href = $this.attr('href');
             if (href) {
+                // If it's an external link, open it in a new tab
                 if (href.match(/^(https?:)?\/\//)) {
                     $this.attr('target', '_blank');
+                // Otherwise, wire it up
                 } else {
                     $this.attr('href', "javascript:___.push('" + escape(href) + "')");
                 }
             }
+        });
+
+        // Hook into each form and perform submissions via AJAX instead
+        $('form').on('submit', function(event) {
+            event.preventDefault();
+            event.stopPropagation();
+
+            var $this = $(this);
+            ___.ajax({
+                url: $this.attr('action'),
+                type: $this.attr('method'),
+                data: $this.formSerialize(),
+            });
         });
     };
 
